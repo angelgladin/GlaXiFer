14a8a520a66673a29ccd1be6c359ed7d1cea2dd7
Ferch2109 <marifer.gusanito@gmail.com>
2017-09-22 13:24:55 -0500

    En proceso de terminar interp

--- GlaXiFer_P03/interp.rkt
+++ GlaXiFer_P03/interp.rkt
@@ -6,7 +6,22 @@
 ;; Función encargada de interpretar el árbol de sintaxis abstracta generado por el parser.
 ;; interp: WAE -> number
 (define (interp exp)
-   (error 'interp "Función no implementada."))
+   (match exp
+    [(num n) n]
+    [(id v) (error 'interp "Identificador libre")]
+    [(op f (cons x xs)) (if (empty? (cdr xs))
+                               (f (interp x) (interp (car xs)))
+                               (f (interp x) (interp (op f xs))))]
+    [(with l bound-body)
+         (interp (foldr (λ (l e) (subst bound-body (binding-name l) (num (interp (binding-value l) )))) '() l ))]
+    [(with* l bound-body)
+         (interp (foldr (λ (l) (subst bound-body (binding-name l) (num (interp (list (binding-value l))))))
+                        '()
+                        (interp l)))]
+    [(cons x xs) (list (subst xs (binding-name x) (binding-value x)) (interp (cons (car xs) (cdr xs))))]
+     ))
+
+;(interp (with* (list (binding 'a (num 0)) (binding 'b (num 1))) (op + (list (id 'a) (id 'b)))))
 
 ;; Función que implementa el algoritmo de sustitución.
 ;; subst: WAE symbol WAE -> WAE
@@ -26,5 +41,4 @@
          (with* (foldr (λ (v l) (cons (binding (binding-name v) (subst (binding-value v) sub-id val) ) l)) '() (cons x xs))
                (if(symbol=? (binding-name x) sub-id)
                      bound-body 
-                     (subst bound-body sub-id val)))]
-    ))
+                     (subst bound-body sub-id val)))]))
