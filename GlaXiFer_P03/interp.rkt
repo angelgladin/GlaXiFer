f1baa808c2b105b70d0b5ceace6091034909baab
Ferch2109 <marifer.gusanito@gmail.com>
2017-09-30 22:29:52 -0500

    YA QUEDO POR FIN INTERP DIO MIO

--- GlaXiFer_P03/interp.rkt
+++ GlaXiFer_P03/interp.rkt
@@ -8,24 +8,24 @@
 (define (interp exp)
    (match exp
     [(num n) n]
-    [(id v) (error 'interp "Identificador libre")]
+    [(id v)  (error 'interp "Identificador libre")]
     [(op f (cons x xs)) (if (empty? (cdr  xs))
                             (f (interp x) (interp (car xs)))
                             (let ([r  (f (interp x) (interp (car xs)))])
-                                             (interp (op f (cons (num r) (cdr xs)) ))))]
+                              (interp (op f (cons (num r) (cdr xs)) ))))]
     [(with l bound-body)
-        (interp (foldr (λ (l e) (subst e (binding-name l) (num (interp (binding-value l))) )) bound-body l ))]
+     (interp (foldr (λ (l e)
+                      (subst e (binding-name l) (num (interp (binding-value l))) )) bound-body l ))]
     [(with* l bound-body)
-         (interp (with (interp l) bound-body))]
+     (interp (with (interp l) bound-body))]
 
     [(cons x xs) (cond
                    [(empty? xs) (cons x xs)]
-                   [(empty? (cdr xs))
-                         (cons (binding (binding-name (car xs)) (subst (binding-value (car xs)) (binding-name x) (binding-value x))) empty)]
-                   [(cons x (interp (cdr (cons x (map (λ (v) (binding (binding-name v) (subst (binding-value v) (binding-name x) (binding-value x)))) xs)))))]
-                 )]
-
-    ))
+                   [(cons x (interp (map (λ (v) (binding (binding-name v)
+                                                                    (subst (binding-value v)
+                                                                           (binding-name x)
+                                                                           (binding-value x)))) xs)))
+                    ])]))
 
 
 ;; Función que implementa el algoritmo de sustitución.
@@ -38,12 +38,14 @@
                 expr)]
     [(op f (cons x xs)) (op f (foldr (λ (v l) (cons (subst v sub-id val) l)) '() (cons x xs)))]
     [(with (cons x xs) bound-body)
-         (with (foldr (λ (v l) (cons (binding (binding-name v) (subst (binding-value v) sub-id val) ) l)) '() (cons x xs))
+         (with (foldr (λ (v l) (cons (binding (binding-name v)
+                                              (subst (binding-value v) sub-id val) ) l)) '() (cons x xs))
                (if(symbol=? (binding-name x) sub-id)
                      bound-body
                      (subst bound-body sub-id val)))]
     [(with* (cons x xs) bound-body)
-         (with* (foldr (λ (v l) (cons (binding (binding-name v) (subst (binding-value v) sub-id val) ) l)) '() (cons x xs))
+         (with* (foldr (λ (v l) (cons (binding (binding-name v)
+                                               (subst (binding-value v) sub-id val) ) l)) '() (cons x xs))
                (if(symbol=? (binding-name x) sub-id)
                      bound-body 
                      (subst bound-body sub-id val)))]))
