668e0ae06404dfc4999403a324dc74047dda8bcb
Ferch2109 <marifer.gusanito@gmail.com>
2017-10-17 01:13:17 -0500

    Solo falta corregir appS

--- GlaXiFer_P04/parser.rkt
+++ GlaXiFer_P04/parser.rkt
@@ -2,11 +2,43 @@
 
 (require "grammars.rkt")
 
+;; [ Auxiliar ]. Función que hace un mapeo entre los operadores en
+;; sintaxis concreta y los operadores de Racket . Esto con el fin de
+;; apli car la operación más adelante .
+;; elige : symbol - > procedure
+(define (elige sexp)
+  (match sexp
+    ['+ +]
+    ['- -]
+    ['* *]
+    ['/ /]
+    ['% modulo]
+    ['min min]
+    ['max max]
+    ['pow mexpt]))
+
+;; Potencia multiparamétrica.
+;; Función que recibe n elementos, a diferencia expt que solo recibe 2 elementos.
+(define mexpt
+  (λ nums (foldr expt (car nums) (cdr nums))))
+
 ;; Analizador sintáctico para WAE.
 ;; Dada una s-expression, construye el árbol de sintaxis abstracta correspondiente.
 ;; parse: s-expression -> FWBAE
 (define (parse sexp)
-   (error 'parse "Función no implementada."))
+   (match sexp
+     [(? symbol?) (idS sexp)]
+     [(? number?) (numS sexp)]
+     [(list 'with (cons x xs) body)
+     (withS (foldr (λ (v l) (cons (binding (first v) (parse (second v))) l)) '() (cons x xs)) (parse body))]
+     [(list 'with* (cons x xs) body)
+     (withS* (foldr (λ (v l) (cons (binding (first v) (parse (second v))) l)) '() (cons x xs)) (parse body))]
+     [(list 'fun (cons x xs) body)
+      (funS (cons x xs) (parse body))]
+     [(list 'app fun-expr args)
+      (appS (parse fun-expr) (parse args))]
+     [(cons x xs)
+      (opS (elige x) (foldr (λ (v l) (cons (parse v) l)) '() xs))]))
 
 ;; Función que elimina el azúcar sintáctica de las expresiones de FWBAE, es decir las convierte a 
 ;; expresiones de FBAE.
