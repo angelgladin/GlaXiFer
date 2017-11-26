e49f20585f2054d750744d353f01a6616aeacbe6
angelgladin <angelgladin@gmail.com>
2017-11-26 01:04:18 -0600

    Se acabó parse y desugar

--- GlaXiFer_P07/parser.rkt
+++ GlaXiFer_P07/parser.rkt
@@ -2,14 +2,144 @@
 
 (require "grammars.rkt")
 
+
+;; Función auxiliar que hace un mapeo entre los operadores en
+;; sintaxis concreta y los operadores de Racket . Esto con el fin de
+;; apli car la operación más adelante .
+;; elige : symbol - > procedure
+(define (elige sexp)
+  (match sexp
+    ['+ +]
+    ['- -]
+    ['* *]
+    ['/ /]
+    ['% mmodulo]
+    ['min min]
+    ['max max]
+    ['pow mexpt]
+    ['zero? zero?]
+    ['not not]
+    ['and and-aux]
+    ['or or-aux]
+    ['< <]
+    ['> >]
+    ['<= <=]
+    ['>= >=]
+    ['= =]
+    ['/= not-eq]
+    ['head car]
+    ['tail cdr]
+    ['empty? empty?]))
+
+;; Función auxiliar auxiliar que hace que hace un `or` que recibe n argumentos
+;; se hizo porque la función `or` es un macro y no una función.
+(define (or-aux . bools)
+  (ormap (λ (x) x) bools))
+
+;; Función auxiliar que hace que hace un `and` que recibe n argumentos
+;; se hizo porque la función `and` es un macro y no una función.
+(define (and-aux . bools)
+  (andmap (λ (x) x) bools))
+
+;; Función auxiliar que hace que hace un `/=` que recibe n argumentos
+;; se hizo porque la función `/=` es un macro y no una función.
+(define (not-eq . params)
+  (not (apply = params)))
+
+;; Potencia multiparamétrica.
+;; Función que recibe n elementos, a diferencia `expt` que solo recibe 2 elementos.
+(define (mexpt . params)
+  (opMultiparametrica expt params))
+
+;; Módulo multiparamétrico.
+;; Función que recibe n elementos, a diferencia de `modulo` que solo recibe 2 elementos.
+(define (mmodulo . params)
+  (opMultiparametrica modulo params))
+
+;; Función auxiliar que dada una función que solo recibe dos parámetros la convierte
+;; en una función asociativa que recibe n parámetros y siempre va asociando
+;; el resultado con el siguiente elemento.
+;; opMultiparametrica: procedure -> list number -> number
+(define (opMultiparametrica f params)
+  (letrec ([aux (λ (n l)
+                  (match l
+                    ['() n]
+                    [(cons x xs) (aux (f n x) xs)]))])
+    (aux (f (first params) (second params)) (cdr (cdr params)))))
+
 ;; Analizador sintáctico para ERCFWBAEL/L.
 ;; Dada una s-expression, costruye el árbol de sintaxis abstracta correspondiente.
 ;; parse: s-expression -> ERCFWBAEL/L.
 (define (parse sexp)
-	(error 'parse "Función no implementada."))
+  (match sexp
+    [(? symbol?) (case sexp ; Símbolo
+                   ['true (boolS #t)] ; Booleano verdadero
+                   ['false (boolS #f)] ; Booleano false
+                   [else (idS sexp)])] ; Identificador
+    [(cons 'list elems) (listS (map parse elems))] ; Lista
+    [(? number?) (numS sexp)] ; Número
+    [(list 'with bindings body) ; With
+     (withS (aux-parse-bindingS bindings) (parse body))]
+    [(list 'with* bindings body) ; With*
+     (withS* (aux-parse-bindingS bindings) (parse body))]
+    [(list 'rec bindings body) ; Rec
+     (recS (aux-parse-bindingS bindings) (parse body))]
+    [(list 'fun (cons x xs) body) ; Función
+     (funS (cons x xs) (parse body))]
+    [(list (list 'fun fun-params fun-body) params) ; Aplicación de función
+     (appS (funS fun-params (parse fun-body)) (map parse params))]
+    [(list 'if cond-expr then-expr else-expr) ; If
+     (ifS (parse cond-expr) (parse then-expr) (parse else-expr))]
+    [(cons 'cond conditions) ; Cond
+     (condS (aux-parse-conds conditions))]
+    [(list 'throwS exception-id) (throwsS exception-id)] ; Throws
+    [(list 'try/catch bindings body) ; Try/catch
+     (try/catchS (aux-parse-bindingS bindings) (parse body))]
+    [(cons x xs) ; Aquí hay dos casos; Una operación o aplicación de función
+     (case x
+       ; Checamos si es una operación del lenguaje.
+       [(+ - * / % min max pow zero? not and or < > <=  >= = /= head tail empty?)
+        (opS (elige x) (map parse xs))]
+       ; Aplicación de función
+       [else (appS (parse x) (map parse xs))])]))
+
+;; Función auxiliar que hace un crea una lista de `bindings`.
+;; aux-parse-bindings list list symbol -> list BindingS
+(define (aux-parse-bindingS bindings)
+  (map (λ (v) (bindingS (first v) (parse (second v)))) bindings))
+
+;; Función auxiliar que hace maneja cada posible valor que puede tener un condition
+;; aux-parse-conds list symbol -> list Condition
+(define (aux-parse-conds conditions)
+  (let ([conditions-map (λ (c) (match c
+                                 [(list 'else else-expr) (else-cond (parse else-expr))]
+                                 [(list expr then-expr) (condition (parse expr) (parse then-expr))]))])
+    (map (λ (v) (conditions-map v)) conditions)))
 
 ;; Función que elimina el azúcar sintáctica de las expresiones de ERCFWBAEL/L, es decir las convierte a 
 ;; expresiones de ERCFBAEL/L.
 ;; desugar: ERCFWBAEL/L -> ERCFBAEL/L
 (define (desugar expr)
-   (error 'desugar "Función no implementada."))
+  (match expr
+    [(idS i) (id i)]
+    [(numS n) (num n)]
+    [(boolS b) (bool b)]
+    [(listS elems) (lisT (map desugar elems))]
+    [(opS f args) (op f (map desugar args))]
+    [(ifS expr then-expr else-expr) (iF (desugar expr) (desugar then-expr) (desugar else-expr))]
+    [(condS (cons x xs)) (match x
+                           [(condition expr then-expr) (desugar (ifS expr then-expr (condS xs)))]
+                           [(else-cond else-expr)  (desugar else-expr)])]
+    [(withS bindings body) (app (fun (map bindingS-name bindings) (desugar body))
+                                (map (λ (v) (desugar (bindingS-value v))) bindings))]
+    [(withS* (cons x xs) body) (desugar (withS (list x) (if (empty? xs) body (withS* xs body))))]
+    [(recS bindings body) (rec (aux-parse-binding bindings) (desugar body))]
+    [(funS params body) (fun params (desugar body))]
+    [(appS fun-expr args) (app (desugar fun-expr) (map desugar args))]
+    [(throwsS exception-id) (throws exception-id)]
+    [(try/catchS bindings body) (try/catch (aux-parse-binding bindings) (desugar body))]))
+
+;; Función auxiliar que hace un crea una lista de `bindings`.
+;; aux-parse-bindings list list symbol -> list BindingS
+(define (aux-parse-binding bindings)
+  (map (λ (v) (binding (bindingS-name v) (desugar (bindingS-value v)))) bindings))
